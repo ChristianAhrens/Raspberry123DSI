956550f8b035b5ef85b06da5396864a1871dc850
Christian Ahrens <christianahrens@me.com>
2026-06-24 17:55:03 +0200

    Updated with latest proto config assets

--- OS_config/launchAndLayout_2026-04-12.sh
+++ OS_config/launchAndLayout_2026-04-12.sh
@@ -3,11 +3,11 @@
 # Launch apps sequentially, waiting for each to register before starting the next
 MemaMo --noconfigui --noupdates &
 sleep 1
-Umsci --noupdates &
+Umsci --noupdates --noconfigui &
 sleep 1
 MemaRe --noconfigui --noupdates &
 sleep 2
 
-swaymsg '[class="Mema.Mo"] resize set width 30ppt'
-swaymsg '[class="Umsci"] resize set width 45ppt'
-swaymsg '[class="Mema.Re"] resize set width 25ppt'
+swaymsg '[class="Mema.Mo"] resize set width 25ppt'
+swaymsg '[class="Umsci"] resize set width 55ppt'
+swaymsg '[class="Mema.Re"] resize set width 20ppt'
